5733ddf337a8e5ed786cab04e23558e5602c6698
1204792945@qq.com <Gxj001130>
2019-05-25 22:16:59 +0800

    增加注释

--- mathy.go
+++ mathy.go
@@ -1,18 +1,20 @@
 package mathy
 
+// 斐波那契数列
 func Fib(n int) int64 {
 	if n <= 1 {
 		return 1
 	}
-	var s = make([]int64, n + 1)
+	var s = make([]int64, n+1)
 	s[0] = 1
 	s[1] = 1
-	for i := 2; i <= n ; i++ {
+	for i := 2; i <= n; i++ {
 		s[i] = s[i-1] + s[i-2]
 	}
 	return s[n]
 }
 
+// 阶乘
 func Fact(n int) int64 {
 	if n <= 1 {
 		return 1
