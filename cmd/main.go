65e1e9da3c02973f116d59b206c54b671812e093
royce_zhu <wenxi.zhu@bolemart.com>
2019-06-13 16:51:45 +0800

    增加结构体信息

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,15 @@ import (
 	mathy "github.com/overanalyze/awsome-mathy"
 )
 
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
 	// Fxxk
 	fmt.Println(mathy.Fib(10))
 	fmt.Println(mathy.Fact(10))
+	person := Person{"zhuguo", 24}
+	fmt.Println(person)
 }
